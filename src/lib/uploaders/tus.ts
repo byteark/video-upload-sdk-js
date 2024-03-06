9565309bf782ae1e956b9badd9a09dd2e8d6f850
Patcharapol Nirunpornphutta <patcharapol@inox.co.th>
2024-03-06 09:14:06 +0700

    Refactor code

--- src/lib/uploaders/tus.ts
+++ src/lib/uploaders/tus.ts
@@ -65,29 +65,16 @@ export class TusUploader implements UploaderInterface {
 
   async pause(): Promise<UploadJob> {
     return new Promise<UploadJob>((resolve) => {
-      try {
-        this.currentUploader.abort(false);
-      } catch (error) {
-        // Likely to be "423 Locked" error, but it's already aborted.
-        console.error(error);
-      } finally {
-        this.job.status = 'paused';
-        resolve(this.job);
-      }
+      this.currentUploader.abort(false);
+      resolve(this.job);
     });
   }
 
   async resume(): Promise<UploadJob> {
     return new Promise<UploadJob>((resolve) => {
-      try {
-        this.currentUploader.start();
-      } catch (error) {
-        // Likely to be "423 Locked" error, but it's already aborted.
-        console.error(error);
-      } finally {
-        this.job.status = 'uploading';
-        resolve(this.job);
-      }
+      this.currentUploader.start();
+      this.job.status = 'uploading';
+      resolve(this.job);
     });
   }
 
